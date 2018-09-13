17c65d44f842429e8d36cb9a84b483dc29c73714
komuW <komuw05@gmail.com>
2018-09-13 10:45:23 +0300

    cool

---
+++ setup.py
@@ -0,0 +1,77 @@
+import os
+import ast
+
+from setuptools import setup, find_packages
+
+# To use a consistent encoding
+import codecs
+
+setup(
+    name="hey",
+    # Versions should comply with PEP440.  For a discussion on single-sourcing
+    # the version across setup.py and the project code, see
+    # https://packaging.python.org/en/latest/single_source_version.html
+    version="0.0.1",
+    description="hey hey",
+    long_description="hey hey hey"
+    
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        "Development Status :: 4 - Beta",
+        # Indicate who your project is intended for
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Security",
+        "Topic :: System :: Installation/Setup",
+        "Topic :: System :: Networking",
+        "Topic :: System :: Systems Administration",
+        "Topic :: Utilities",
+        # Pick your license as you wish (should match "license" above)
+        "License :: OSI Approved :: MIT License",
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        "Programming Language :: Python :: 3.6",
+    ],
+    # What does your project relate to?
+    keywords="hey",
+    # You can just specify the packages manually here if your project is
+    # simple. Or you can use find_packages().
+
+    packages=find_packages(exclude=["docs", "*tests*", "examples"]),
+    # Alternatively, if you want to distribute just a my_module.py, uncomment
+    # this:
+    #   py_modules=["my_module"],
+    # List run-time dependencies here.  These will be installed by pip when
+    # your project is installed. For an analysis of "install_requires" vs pip's
+    # requirements files see:
+    # https://packaging.python.org/en/latest/requirements.html
+    install_requires=[],
+    # List additional groups of dependencies here (e.g. development
+    # dependencies). You can install these using the following syntax,
+    # for example:
+    # $ pip install -e .[dev,test]
+    extras_require={
+        "dev": ["twine", "wheel"],
+        "test": [
+            "flake8==3.5.0",
+        ],
+    },
+)
+
+# python packaging documentation:
+# 1. https://python-packaging.readthedocs.io/en/latest/index.html
+# 2. https://python-packaging-user-guide.readthedocs.io/tutorials/distributing-packages
+# a) pip install wheel twine
+# b) pip install -e .
+# c) python setup.py sdist
+# d) python setup.py bdist_wheel
+# e) DONT use python setup.py register and python setup.py upload. They use http
+# f) twine upload dist/* -r testpypi
+# g) pip install -i https://testpypi.python.org/pypi <package name>
+# h) twine upload dist/*   # prod pypi
+# i) pip install <package name>
+# pip install -e .[dev,test]
